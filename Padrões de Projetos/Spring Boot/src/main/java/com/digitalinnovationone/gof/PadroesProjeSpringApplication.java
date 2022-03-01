f00c0d504776026974a503b9a5771157bcf7e6a9
luizfilipebp <luizfilipe098@gmail.com>
2022-02-28 23:45:45 -0300

    Aplicando os padrões no Springboot

---
+++ Padrões de Projetos/Spring Boot/src/main/java/com/digitalinnovationone/gof/PadroesProjeSpringApplication.java
@@ -0,0 +1,15 @@
+package com.digitalinnovationone.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class PadroesProjeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjeSpringApplication.class, args);
+	}
+
+}
---
+++ Padrões de Projetos/Spring Boot/src/test/java/com/digitalinnovationone/gof/PadroesProjeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
